b1d2ce62a2a402900b054a79048089be05803a85
backduu <power4290@gmail.com>
2025-09-15 16:14:10 +0900

    [chat] 채팅방 기능 개선
    공개방 처리 + 참여자 중에 LEFT, BANNED인 참여자는 조회안되도록 수정

--- src/main/java/com/agit/peerflow/repository/ChatParticipantRepository.java
+++ src/main/java/com/agit/peerflow/repository/ChatParticipantRepository.java
@@ -3,6 +3,7 @@ package com.agit.peerflow.repository;
 import com.agit.peerflow.domain.entity.ChatParticipant;
 import com.agit.peerflow.domain.entity.ChatRoom;
 import com.agit.peerflow.domain.entity.User;
+import com.agit.peerflow.domain.enums.ChatRoomType;
 import com.agit.peerflow.domain.enums.ParticipantType;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,7 +17,15 @@ import java.util.Optional;
 
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
     // 특정 방의 모든 참여자
-    @Query("SELECT cp FROM ChatParticipant cp JOIN FETCH cp.user WHERE cp.chatRoom = :chatRoom")
+    @Query(
+            """
+                SELECT cp 
+                  FROM ChatParticipant cp 
+            JOIN FETCH cp.user
+                 WHERE cp.chatRoom = :chatRoom
+                   AND cp.status = com.agit.peerflow.domain.enums.ParticipantType.ACTIVE
+            """
+    )
     List<ChatParticipant> findByChatRoomWithUser(@Param("chatRoom") ChatRoom chatRoom);
 
     // 특정 유저가 특정 방에 있는지 확인
