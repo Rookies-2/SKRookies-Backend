274849e1362f2e3dc89e9b4398a2ec71c595bee0
backduu <power4290@gmail.com>
2025-09-16 16:12:18 +0900

    [submissions] 제출 url, 텍스트내용 DTO 세팅

--- src/main/java/com/agit/peerflow/dto/assignment/AssignmentDetailResponseDTO.java
+++ src/main/java/com/agit/peerflow/dto/assignment/AssignmentDetailResponseDTO.java
@@ -22,7 +22,6 @@ public class AssignmentDetailResponseDTO {
     private final String title;
     private final String description;
     private final String creatorName;
-    //private final String textContext;
     private final LocalDateTime createdAt;
     private final LocalDateTime dueDate;
     private final List<String> attachmentUrls;
@@ -34,7 +33,6 @@ public class AssignmentDetailResponseDTO {
             String title,
             String description,
             String creatorName,
-          //  String textContext,
             LocalDateTime createdAt,
             LocalDateTime dueDate,
             List<String> attachmentUrls,
@@ -45,7 +43,6 @@ public class AssignmentDetailResponseDTO {
                 .title(title)
                 .description(description)
                 .creatorName(creatorName)
-              //  .textContext(textContext)
                 .createdAt(createdAt)
                 .dueDate(dueDate)
                 .attachmentUrls(attachmentUrls)
